7821eb0010559c2598b86210fda15ee96426c41d
Tauriq Hendricks <tauriqh786@gmail.com>
2020-08-13 10:16:53 +0200

    Added product-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductListComponent
+   ],
   imports: [
     BrowserModule
   ],
